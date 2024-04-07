8510532bc9d94d8f124137720a5959f4360d1656
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-07 13:55:36 +0000

    alhamdo li Allah

--- python/c/cross-compile/bismi_allah.c
+++
@@ -1,9 +0,0 @@
-//in the name of Allah
-
-#include <stdio.h>
-
-int main()
-{
-    printf("in the name of Allah\n");
-}
-
--- python/lua/bismi_allah.lua
+++
@@ -1,7 +0,0 @@
---in the name of Allah
-
-print("[lua] in the name of Allah")
-
-
-print("[lua] " .. bismi_allah_table.bismi_allah_subtable.var)
-
