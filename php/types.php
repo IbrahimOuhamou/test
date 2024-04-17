88fa2921c57b44c102d4a557de668a7d07c034fd
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-17 18:01:40 +0100

    alhamdo li Allah

---
+++ php/types.php
@@ -0,0 +1,9 @@
+<?php
+//in the name of Allah
+
+echo "in the name of Allah\n";
+
+echo ('0' == 0 ? '\'0\' == 0' : '\'0\' != 0 ') . "\n";
+echo ('0' === 0 ? '\'0\' === 0' : '\'0\' !== 0 ') . "\n";
+
+
