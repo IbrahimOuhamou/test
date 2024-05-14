5dedb03f502653689f8dee816ff1062298f4fa8b
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-14 13:18:13 +0100

    alhamdo li Allah

--- sdl/src/main.zig
+++ sdl/src/main.zig
@@ -15,24 +15,40 @@ pub fn main() !void {
     _ = c.SDL_Init(c.SDL_INIT_VIDEO);
     const window = c.SDL_CreateWindow("la ilaha illa Allah", c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, 1200, 800, c.SDL_WINDOW_SHOWN | c.SDL_WINDOW_RESIZABLE);
     if (null == window) {
-        c.SDL_LogError("Error c.SDL_CreateWindow() %s\n", c.SDL_GetError());
+        c.SDL_LogError(0, "Error c.SDL_CreateWindow() %s\n", c.SDL_GetError());
         unreachable;
     }
     const renderer = c.SDL_CreateRenderer(window, -1, c.SDL_RENDERER_PRESENTVSYNC);
     if (null == renderer) {
         c.SDL_Log("Error c.SDL_CreateRenderer() %s\n", c.SDL_GetError());
+        unreachable;
     }
+
+    var bismi_allah_rect = c.SDL_Rect{
+        .x = 10,
+        .y = 10,
+        .w = 30,
+        .h = 30,
+    };
+
     var running: bool = true;
     while (running) {
         var event: c.SDL_Event = undefined;
-        while (c.SDL_PollEvent(&event)) {}
-        switch (event.type) {
-            c.SDL_QUIT => running = false,
+        while (c.SDL_PollEvent(&event) != 0) {
+            if (c.SDL_QUIT == event.type) {
+                running = false;
+            } else if (c.SDL_KEYDOWN == event.type) {
+                bismi_allah_rect.w += 10;
+                if(c.SDLK_RIGHT == )
+                try stdout.print("alhamdo li Allah event: {any}\n", .{event.key.keysym.scancode});
+            }
         }
 
-        c.SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
-        c.SDL_RenderClear(renderer);
-        c.SDL_RenderPresent(renderer);
+        _ = c.SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+        _ = c.SDL_RenderClear(renderer);
+        _ = c.SDL_SetRenderDrawColor(renderer, 30, 30, 200, 100);
+        _ = c.SDL_RenderFillRect(renderer, &bismi_allah_rect);
+        _ = c.SDL_RenderPresent(renderer);
     }
     c.SDL_DestroyRenderer(renderer);
     c.SDL_DestroyWindow(window);
