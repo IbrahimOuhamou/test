fef527f452cff741ba85cf6f1a525de6c25a1769
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-05-29 18:54:38 +0100

    alhamdo li Allah

--- c/bismi_allah/bismi_allah.c
+++ c/bismi_allah/bismi_allah.c
@@ -6,7 +6,7 @@ int main()
 {
     printf("in the name of Allah\n");
 
-    for(int i =0; i < 123; i++)
+    for(int i =0; i < 12; i++)
     {
         printf("in the name of Allah\nla ilaha illa Allah mohammed rassoul Allah\n");
     }
